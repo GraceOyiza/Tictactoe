d0ed6ac82c25232e359463754a5b24bb34c6ead0
GraceOyiza <graceoyiza13@gmail.com>
2020-09-10 13:06:08 +0100

    Add welcome message

--- bin/main.rb
+++ bin/main.rb
@@ -8,3 +8,13 @@ board_o = Board.new
 game_o = Game.new
 palyer_o = Player.new
 
+puts "\n\n"
+puts '           Welcome,  TiC TaC To\'eR!'
+puts ''
+board_o.display_demo_board
+puts ''
+sleep(1)
+puts "          Built by Robert and Grace\n\n\n"
+
+sleep(1)
+start_msg = "            Press \'S\' to start!" # Start Game
