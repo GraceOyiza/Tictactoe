d316a86e749fc73a6fa81f81518a42997dc70e65
GraceOyiza <graceoyiza13@gmail.com>
2020-09-10 12:34:25 +0100

    Add check_player1_name method

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,18 @@ class Player
     $player_1 = ''
     $player_2 = ''
 
-
+    def check_player1_name
+        sleep(1)
+        puts '       Player [1], enter your name' 
+        $player_1 = gets.chomp.capitalize!
+        if $player_1 != nil  #|| $player_1.scan(/\D/) != empty
+          sleep(1)
+          puts "              #{$player_1}, ready to go!\n\n"
+        else
+          puts "Please enter a correct name"
+          check_player1_name
+          puts "\n"
+        end
+      end
 
 end
