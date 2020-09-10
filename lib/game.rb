1aa2e43c7d8f6a7654e0ad4cba79fc14d5e4c825
Robert Loterh <rloterh@gmail.com>
2020-09-10 11:42:30 +0000

    Add start_check method

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,15 @@
 class Game
 
+  def start_check
+    start_game = gets.strip.downcase
+    sleep(1)
+    if start_game == 's'
+      puts "             OK, let\'s Play!\n\n\n"
+    else
+      puts "             PLEASE INPUT 'S' TO CONTINUE..\n"
+      start_game = nil?
+      start_check
+    end
+  end
+
 end
