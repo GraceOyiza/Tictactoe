4d4047e411badd652430ba515c60db2e5c82edeb
Robert Loterh <rloterh@gmail.com>
2020-09-10 11:27:13 +0000

    Add determine_winner method

--- lib/board.rb
+++ lib/board.rb
@@ -33,4 +33,24 @@ class Board
         puts '      .-----.-----.-----.      .-----.-----.-----.'
       end
 
+      def determine_winner
+        if $board[0] != ' ' && $board[1] != ' ' && $board[2] != ' ' && $board[0] == $board[1] && $board[1] == $board[2] # 1 2 3
+          $winning_move = true
+        elsif $board[0] != ' ' && $board[4] != ' ' && $board[8] != ' ' && $board[0] == $board[4] && $board[4] == $board[8] # 1 5 9
+          $winning_move = true
+        elsif $board[0] != ' ' && $board[3] != ' ' && $board[6] != ' ' && $board[0] == $board[3] && $board[3] == $board[6] # 1 4 7
+            $winning_move = true
+        elsif $board[1] != ' ' && $board[4] != ' ' && $board[7] != ' ' && $board[1] == $board[4] && $board[4] == $board[7] # 2 5 8
+            $winning_move = true
+        elsif $board[2] != ' ' && $board[4] != ' ' && $board[6] != ' ' && $board[2] == $board[4] && $board[4] == $board[6] # 3 5 7
+            $winning_move = true
+        elsif $board[2] != ' ' && $board[5] != ' ' && $board[8] != ' ' && $board[2] == $board[5] && $board[5] == $board[8] # 3 6 9
+            $winning_move = true 
+        elsif $board[3] != ' ' && $board[4] != ' ' && $board[5] != ' ' && $board[3] == $board[4] && $board[4] == $board[5] # 4 5 6
+            $winning_move = true
+        elsif $board[6] != ' ' && $board[7] != ' ' && $board[8] != ' ' && $board[6] == $board[7] && $board[7] == $board[8] # 7 8 9
+            $winning_move = true
+        end
+      end
+
 end
