395e4579989535d05d1775b58fe8baee0c9f91cc
Robert Loterh <rloterh@gmail.com>
2020-09-10 11:46:37 +0000

    Add player_2_turn

--- lib/game.rb
+++ lib/game.rb
@@ -38,6 +38,30 @@ class Game
     end
   end
 
+  def player_2_turn
+    $board_input = nil?
+    $board_input = Integer(gets) rescue false
+    if $board_input && $board_input >= 1 && $board_input <= 9 
+      puts "\n You chose cell #{$board_input}"
+      if $board[$board_input - 1] == ' '
+        $board[$board_input - 1] = 'O'
+        puts ''
+        $board_o.board_choice_display
+        $board_o.determine_winner
+        if $winning_move == false
+          $player_turn += 1
+        end
+      else
+        puts "\n\n             Cell #{$board_input} is already chosen\n Choose an empty cell\n\n"
+        player_2_turn
+      end
+    else
+      puts "Please enter an Integer/ Value [1-9]!"
+      $board_input = nil
+      player_2_turn
+    end
+  end
+
   
 
 end
